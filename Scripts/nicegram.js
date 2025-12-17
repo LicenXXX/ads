8981e5e29ba69b36980b023aaf2864251c213e9c
LicenXXX <114461931+LicenXXX@users.noreply.github.com>
2025-12-17 21:10:34 +0800

    Add files via upload

---
+++ Scripts/nicegram.js
@@ -0,0 +1,4 @@
+hostname = nicegram.cloud
+
+[Rewrite]
+^https?:\/\/nicegram\.cloud\/api\/v\d\/(ai-assistant\/purchase-list|user\/info|telegram\/auth) url script-response-body https://raw.githubusercontent.com/LicenXXX/ads/refs/heads/main/Scripts/nicegramvip.js
